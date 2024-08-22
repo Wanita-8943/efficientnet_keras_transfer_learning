7949aa69895288b8894134a70cac867cc2f3f686
NITA <76930981+Wanita-8943@users.noreply.github.com>
2024-08-23 02:27:06 +0700

    layers.py

--- efficientnet/layers.py
+++ efficientnet/layers.py
@@ -5,40 +5,59 @@ from tensorflow.keras.utils import get_custom_objects
 
 
 class Swish(KL.Layer):
+    """
+    Swish activation function layer.
+    """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
     def call(self, inputs, **kwargs):
+        """
+        Applies the Swish activation function to the inputs.
+        """
         return tf.nn.swish(inputs)
 
 
 class DropConnect(KL.Layer):
-
+    """
+    DropConnect regularization layer.
+    
+    DropConnect randomly drops connections during training with a specified rate.
+    """
     def __init__(self, drop_connect_rate=0., **kwargs):
         super().__init__(**kwargs)
         self.drop_connect_rate = drop_connect_rate
 
     def call(self, inputs, training=None):
-
+        """
+        Applies DropConnect to the inputs.
+        
+        Arguments:
+        inputs -- Tensor input to the layer.
+        training -- Boolean, whether the layer should behave in training mode or inference mode.
+        
+        Returns:
+        Tensor output after applying DropConnect.
+        """
         def drop_connect():
             keep_prob = 1.0 - self.drop_connect_rate
-
-            # Compute drop_connect tensor
-            batch_size = tf.shape(inputs)[0]
-            random_tensor = keep_prob
-            random_tensor += tf.random.uniform([batch_size, 1, 1, 1], dtype=inputs.dtype)
+            shape = tf.shape(inputs)
+            random_tensor = keep_prob + tf.random.uniform(shape, dtype=inputs.dtype)
             binary_tensor = tf.floor(random_tensor)
-            output = tf.divide(inputs, keep_prob) * binary_tensor
-            return output
-
+            return tf.divide(inputs, keep_prob) * binary_tensor
 
         return K.in_train_phase(drop_connect, inputs, training=training)
 
     def get_config(self):
+        """
+        Returns the configuration of the layer as a dictionary.
+        """
         config = super().get_config()
         config['drop_connect_rate'] = self.drop_connect_rate
         return config
 
 
+# Register the custom layers with Keras
 get_custom_objects().update({
     'DropConnect': DropConnect,
     'Swish': Swish,
